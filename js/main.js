b43cdac03a1884164bae5f0f53a8a335d43f60c1
Melanie Benedito <mel130567@gmail.com>
2021-08-27 11:33:33 -0300

    primeras funcionalidades

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 
-/* El proceso principal del proyecto sera la simulacion de reserva de turnos mediante un form*/ 
-function saludar (){
-    let nombre = prompt("Porfavor ingresa tu nombre");
-    alert(`¡Bienvenid@ ${nombre} en nuestra web podras reservar turno para entrenar con nosotros!`);
-}
+/* El proceso principal del proyecto sera la simulacion de reserva de turnos mediante un form*/
 
 //Se declara el objeto "turno"
 
@@ -17,16 +13,55 @@ class Turno{
         this.fecha = fecha;
         this.hora = hora;
     }
-    reservar (opcion){
-        if(opcion == "si"){
-            console.log(` ${this.nombre} tu turno para la clase de ${this.clase} el día ${this.fecha} a las ${this.hora} ha sido reservado con exito! `);
-        }else{
-            console.log(`No has reservado un turno`);
-        }
+}
+
+let btnConfirmar = document.getElementById("btnConfirmar");
+btnConfirmar.addEventListener("click", recogerDatos);
+ 
+
+function verModal(){
+    let modal = document.getElementById("turnoModal");
+    modal.classList.add("show");
+    modal.style = ("display: block;")
+    let mensaje = document.createElement("p");
+    mensaje.innerHTML = `<p>Tu turno ha sido reservado con exito</p>`;
+    document.getElementById("modalBody").appendChild(mensaje);
+
+    let btnCerrar = document.getElementById("btnCerrarModal");
+    btnCerrar.addEventListener("click", modalOff)
+    function modalOff(){ 
+        modal.classList.remove("show")
+        modal.style = ("display: none;")
     }
 }
 
-const turno1 = new Turno ("Melanie", "Benedito", "Mel@mel.com", 111111, "crossfit", "01/09/2021", "10HS");
 
-turno1.reservar ("si");
+function recogerDatos(e){
+    e.preventDefault();
+    let nombre = document.getElementById("nombreForm").value;
+    let apellido = document.getElementById("apellidoForm").value;
+    let email = document.getElementById("emailForm").value;
+    let tel = document.getElementById("telForm").value;
+    let clase;
+    if (document.getElementById("radioCross").checked){
+        clase = document.getElementById("radioCross").value;
+    } else if (document.getElementById("radioFun").checked){
+     clase = document.getElementById("radioFun").value;
+    } 
+    let fecha = document.getElementById("fechaForm").value;
+    let hora = document.getElementById("horariosForm").value;     
+
+    const turno1 = new Turno (`${nombre}`, `${apellido}`, `${email}`, `${tel}`, `${clase}`, `${fecha}`, `${hora}`);
+    const turno1Json = JSON.stringify(turno1);
+
+    localStorage.setItem("turno", turno1Json);
+
+    verModal();
+}
+
+
+
+
+
+
 
